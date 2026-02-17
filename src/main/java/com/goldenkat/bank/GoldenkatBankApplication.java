eeb4bd4af9f13019a36c0bee10d871dc4db73d6d
FortressTechnologiesInc <130385349+FortressTechnologiesInc@users.noreply.github.com>
2026-02-17 09:18:53 -0500

    Create GoldenkatBankApplication.java

---
+++ src/main/java/com/goldenkat/bank/GoldenkatBankApplication.java
@@ -0,0 +1,12 @@
+package com.goldenkat.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenkatBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldenkatBankApplication.class, args);
+    }
+}
